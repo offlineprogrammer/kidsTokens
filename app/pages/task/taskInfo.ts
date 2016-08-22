313d931d88e63284e26b3679eb8e6a0b299cf941
offlineprogrammer <mmalaka@gmail.com>
2016-08-22 19:15:04 +0100

    styling and chages

--- app/pages/task/taskInfo.ts
+++ app/pages/task/taskInfo.ts
@@ -1,9 +1,15 @@
 import {
   Component
 } from '@angular/core';
-import { Modal }                                           from 'ionic-angular/components/modal/modal';
 import {
-  NavController, NavParams, ActionSheetController, ModalController   
+  Modal
+} from 'ionic-angular/components/modal/modal';
+import {
+  NavController,
+  NavParams,
+  ActionSheetController,
+  ModalController,
+  Platform
 } from 'ionic-angular';
 
 
@@ -11,14 +17,21 @@ import {
   DataService
 } from '../../services/data';
 
-import { Child }                       from '../../models/child';
-import { Task }                       from '../../models/task';
+import {
+  Child
+} from '../../models/child';
+import {
+  Task
+} from '../../models/task';
 import {
   GAService
 } from '../../services/googleAnalyticsService';
 import {
-    GAEvent
+  GAEvent
 } from '../../models/gaEvent';
+import {
+  NativeAudio
+} from 'ionic-native';
 
 
 @Component({
@@ -26,99 +39,127 @@ import {
 
 })
 export class TaskInfo {
- oChild: Child;
- oTask: Task;
- tokenNumbers: number[];
- tokenstriples: number[];
- 
+  oChild: Child;
+  oTask: Task;
+  tokenNumbers: number[];
+  tokenstriples: number[];
+
   constructor(
     private dataService: DataService,
     private gaService: GAService,
-     private nav: NavController,
+    private nav: NavController,
     private navCtrl: NavController,
     private navParams: NavParams,
-    private modalController : ModalController
+    private modalController: ModalController,
+     private platform: Platform
   ) {
-   this.oTask = navParams.get('task');
-   this.oChild = navParams.get('child');
-   this.tokenNumbers = this.fillArrayWithNumbers(+this.oChild.tokenNumbers); 
-   this.tokenstriples = this.getTriples();
-   this.gaService.trackView('TaskInfo');
-   
+    this.oTask = navParams.get('task');
+    this.oChild = navParams.get('child');
+    this.tokenNumbers = this.fillArrayWithNumbers(+this.oChild.tokenNumbers);
+    this.tokenstriples = this.getTriples();
+    this.gaService.trackView('TaskInfo');
+     platform.ready().then(() => {
+        NativeAudio.preloadSimple('win', 'images/win.mp3').then(function(msg) {
+            console.log(msg);
+          }, function(error) {
+            alert(error);
+          });
+         this.playSound('win');
+     });
+    
+     
+
   }
 
 
-fillArrayWithNumbers(n: number) {
+  fillArrayWithNumbers(n: number) {
     let nArray = [];
-    nArray =  Array.apply(null, Array(n));
-    return nArray.map(function (x, i) { return i; });
-}
+    nArray = Array.apply(null, Array(n));
+    return nArray.map(function(x, i) {
+      return i;
+    });
+  }
 
   getTriples() {
     let triples = [];
     let length = this.tokenNumbers.length;
     for (let i = 0; i < length; i += 2) {
-        let trio = [];
-        trio.push(this.tokenNumbers[i]);
-        if (i + 1 < length) {
-            trio.push(this.tokenNumbers[i + 1]);
-        }
-        triples.push(trio);
+      let trio = [];
+      trio.push(this.tokenNumbers[i]);
+      if (i + 1 < length) {
+        trio.push(this.tokenNumbers[i + 1]);
+      }
+      triples.push(trio);
     }
     return triples;
-}
+  }
 
 
-    private updateData(): void {
-        this.dataService.updateKids()
-            .then(() => {
-              let oGAEvent: GAEvent;
-                    oGAEvent = {
-                        category: 'Task',
-                        action: 'UpdateScore',
-                        label: this.oTask.name,
-                        value: this.oTask.score
-                    };
-                    this.gaService.trackEvent(oGAEvent);
-            });
+  private updateData(): void {
+    this.dataService.updateKids()
+      .then(() => {
+        let oGAEvent: GAEvent;
+        oGAEvent = {
+          category: 'Task',
+          action: 'UpdateScore',
+          label: this.oTask.name,
+          value: this.oTask.score
+        };
+        this.gaService.trackEvent(oGAEvent);
+        
+      });
+  }
+
+  private stopSound(key: string): void {
+    
+    
+  }
+
+  private playSound(key: string): void {
+     NativeAudio.play(key, function(key) {}).then(function(msg) {
+            console.log(msg);
+          }, function(error) {
+            alert(error);
+          });
+    
+  }
+
+  addToken(): void {
+    console.log(this.oTask.score);
+    this.oTask.score++;
+    this.updateData();
+    console.log(this.oTask.score);
+  }
+
+  removeToken(): void {
+    console.log(this.oTask.score);
+    this.oTask.score--;
+    this.updateData();
+    console.log(this.oTask.score);
+  }
+
+  resetScore(): void {
+    this.oTask.score = 0;
+    this.updateData();
+  }
+
+  deleteTask(data: Task): void {
+
+    let index = this.oChild.tasks.indexOf(data);
+
+    if (index > -1) {
+      this.oChild.tasks.splice(index, 1);
     }
+    this.updateData();
 
-    addToken(): void {
-      console.log(this.oTask.score);
-      this.oTask.score++;
-      this.updateData();
-       console.log(this.oTask.score);
-    }
+    this.navCtrl.pop();
 
-    removeToken(): void {
-      console.log(this.oTask.score);
-      this.oTask.score--;
-      this.updateData();
-      console.log(this.oTask.score);
-    }
-
-    resetScore(): void {      
-      this.oTask.score = 0;
-      this.updateData();      
-    }
-
-    deleteTask(data: Task): void {
- 
-        let index = this.oChild.tasks.indexOf(data);
- 
-        if (index > -1) {
-            this.oChild.tasks.splice(index, 1);
-        }
-          this.updateData();      
-
-         this.navCtrl.pop();
-
-    }
+  }
 
 
 
 
-   
 
-  
+
+
 }
